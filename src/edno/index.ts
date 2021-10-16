2eda76f56e144d24abdcff1c40a2131e3c0a51f4
Blopaa <pabloosabaterr@gmail.com>
2021-10-16 16:02:28 +0200

    feat: compatibility with express middlewares

--- src/edno/index.ts
+++ src/edno/index.ts
@@ -8,6 +8,21 @@ import processMiddleware from '../helpers/processMiddleware';
 
 export class Edno {
     private routeTable: { [key: string]: any } = {};
+    private beforeMiddleware: Array<
+        (
+            req: IncomingMessage,
+            res: ServerResponse,
+            next: (err?: unknown) => void
+        ) => void
+    > = [];
+
+    private methodFunction(method: string, path: string, ...rest: any[]) {
+        if (rest.length === 1) {
+            this.registerRoutes(path, rest[0], method);
+        } else {
+            this.registerRoutes(path, rest[1], method, rest[0]);
+        }
+    }
 
     private registerRoutes(
         path: string,
@@ -25,8 +40,27 @@ export class Edno {
         });
     }
 
-    public create() {
+    public use(
+        cb: (
+            req: IncomingMessage,
+            res: ServerResponse,
+            next: (err?: unknown) => void
+        ) => void
+    ): void {
+        this.beforeMiddleware.push(cb);
+    }
+
+    public create(port: number) {
         http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+            this.beforeMiddleware.forEach((m) => {
+                m(<Request>req, <Response>res, (err: any) => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.end('internal error on edno.use middleware');
+                    }
+                    return;
+                });
+            });
             const overrideReq: Request = <Request>req;
             const routes = Object.keys(this.routeTable);
             let match = false;
@@ -52,12 +86,8 @@ export class Edno {
 
                     overrideReq.params = m ? m.groups : undefined;
                     overrideReq.body = await readBody(req);
-                    await processMiddleware(
-                        middleware,
-                        overrideReq,
-                        res
-                    );
-                    const overrideRes = createResponse(<Response>res)
+                    await processMiddleware(middleware, overrideReq, res);
+                    const overrideRes = createResponse(<Response>res);
                     cb(req, overrideRes);
                     match = true;
                     break;
@@ -67,15 +97,20 @@ export class Edno {
                 res.statusCode = 404;
                 res.end('not found');
             }
-        }).listen(3000);
+        }).listen(port);
 
         return {
             get: (path: string, ...rest: any[]) => {
-                if (rest.length === 1) {
-                    this.registerRoutes(path, rest[0], 'get');
-                } else {
-                    this.registerRoutes(path, rest[1], 'get', rest[0]);
-                }
+                this.methodFunction('get', path, ...rest);
+            },
+            post: (path: string, ...rest: any[]) => {
+                this.methodFunction('post', path, ...rest);
+            },
+            put: (path: string, ...rest: any[]) => {
+                this.methodFunction('put', path, ...rest);
+            },
+            delete: (path: string, ...rest: any[]) => {
+                this.methodFunction('delete', path, ...rest);
             },
         };
     }
