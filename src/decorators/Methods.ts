8155bcfb0b660ab50f82eac0a82f8c82a01a282b
Blopaa <pabloosabaterr@gmail.com>
2021-10-20 22:11:46 +0200

    feat: endpoint decorator & store implementation

---
+++ src/decorators/Methods.ts
@@ -0,0 +1,44 @@
+import controllerStore from "../stores/controllerStore";
+import { Methods } from "../types/route";
+
+export function Get(path: string): MethodDecorator {
+ return (target: any, propertyKey: string | symbol): void => {
+   controllerStore.registerEndpoint({
+     path,
+     propertyKey: propertyKey.toString(),
+     method: Methods.Get,
+     controller: target.constructor.name
+   })
+ }
+}
+export function Post(path: string): MethodDecorator {
+  return (target: any, propertyKey: string | symbol): void => {
+    controllerStore.registerEndpoint({
+      path,
+      propertyKey: propertyKey.toString(),
+      method: Methods.Post,
+      controller: target.constructor.name
+    })
+  }
+}
+export function Put(path: string): MethodDecorator {
+  return (target: any, propertyKey: string | symbol): void => {
+    controllerStore.registerEndpoint({
+      path,
+      propertyKey: propertyKey.toString(),
+      method: Methods.Put,
+      controller: target.constructor.name
+    })
+  }
+}
+export function Delete(path: string): MethodDecorator {
+  return (target: any, propertyKey: string | symbol): void => {
+    controllerStore.registerEndpoint({
+      path,
+      propertyKey: propertyKey.toString(),
+      method: Methods.Delete,
+      controller: target.constructor.name
+    })
+  }
+}
+
--- src/stores/controllerStore.ts
+++ src/stores/controllerStore.ts
@@ -1,12 +1,18 @@
-import { ControllerDef, EndpointDef } from "../types/route";
+import { ControllerDef, EndpointDef } from '../types/route';
 
-class ControllerStore {
+class ControllerStore implements Iterable<EndpointDef> {
     private readonly _list = new Map<string, ControllerDef>();
     private readonly endpoints = new Map<string, EndpointDef>();
 
-    public registerEndpoint(endpoint: EndpointDef){
-        const key = `${endpoint.controller}-${endpoint.propertyKey}`
-        this.endpoints.set(key, endpoint)
+    public registerEndpoint(endpoint: EndpointDef) {
+        const key = `${endpoint.controller}-${endpoint.propertyKey}`;
+        this.endpoints.set(key, endpoint);
+    }
+
+    public *[Symbol.iterator](): Iterator<EndpointDef> {
+        for(const endpoint of this.endpoints.values()){
+            yield endpoint;
+        }
     }
 
     public registerRoutes(
@@ -14,7 +20,11 @@ class ControllerStore {
         Controller: new (...args: unknown[]) => unknown
     ) {
         const target = new Controller([]);
-        this._list.set(Controller.name, {path, target})
+        this._list.set(Controller.name, { path, target });
+    }
+
+    public getController (controller: string): ControllerDef | undefined {
+        return this._list.get(controller);
     }
 }
 
