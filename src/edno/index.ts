22c1c15ad16a0cfde80b46c6671288dab948f859
Blopaa <pabloosabaterr@gmail.com>
2021-10-20 22:12:11 +0200

    feat: configRoutes

--- src/edno/index.ts
+++ src/edno/index.ts
@@ -1,33 +1,37 @@
 import * as http from 'http';
-import { Middleware, Options, Request, Response } from "../types/route";
+import { Middleware, Options, Request, Response } from '../types/route';
 import { IncomingMessage, ServerResponse } from 'http';
 import readBody from '../helpers/readBody';
 import { parse } from '../regex/url-to-regex';
 import ResponseBuilder from '../response/responseBuilder';
 import processMiddleware from '../helpers/processMiddleware';
-import { existsSync } from "fs";
-import { readDirRecursive } from "../helpers/readDirRecursive";
+import { existsSync } from 'fs';
+import { readDirRecursive } from '../helpers/readDirRecursive';
+import controllerStore from '../stores/controllerStore';
 
 export class Edno {
     private routeTable: { [key: string]: any } = {};
     private beforeMiddleware: Array<Middleware> = [];
+
     constructor(private options: Options) {
         (async () => {
-            await this.loadControllers()
-        })()
+            await this.loadControllers();
+            this.configRoutes();
+        })();
     }
 
-    private path(path: string): string{
-      return this.options.root ? this.options.root.concat('/', path) : path
+    private path(path: string): string {
+        return this.options.root ? this.options.root.concat('/', path) : path;
     }
 
     private async loadControllers(): Promise<void> {
-        const controllersPath: string = this.path('controllers')
+        const controllersPath: string = this.path('controllers');
         if (!existsSync(controllersPath)) return;
         const files = readDirRecursive(controllersPath);
         for (let x = 0; x < files.length; x++) {
-            await import(files[x])
+            await import(files[x]);
         }
+        console.info(`loaded ${files.length} controllers`);
     }
 
     private methodFunction(method: string, path: string, ...rest: any[]) {
@@ -57,6 +61,37 @@ export class Edno {
 
     public use(cb: Middleware): void {
         this.beforeMiddleware.push(cb);
+    }
+
+    public configRoutes() {
+        const controllers: string[] = [];
+        for (const endpoint of controllerStore) {
+            const controller = controllerStore.getController(
+                endpoint.controller
+            );
+            if (!controller)
+                throw new Error(`Controller ${endpoint.controller} not found`);
+            if (!controllers.includes(endpoint.controller)) {
+                controllers.push(endpoint.controller);
+            }
+            let path = `${controller.path}${
+                controller.path.endsWith('/') && endpoint.path.startsWith('/')
+                    ? endpoint.path.slice(1)
+                    : !endpoint.path.startsWith('/') &&
+                      !controller.path.endsWith('/')
+                    ? '/'.concat(endpoint.path)
+                    : endpoint.path
+            }`;
+            let lastPath = path[path.length - 1];
+            if (lastPath === '/') path = path.substring(1);
+            this.createRoute(
+                endpoint.method.toLowerCase(),
+                path,
+                (controller.target as Record<string, () => unknown>)[
+                    endpoint.propertyKey
+                ].bind(controller.target)
+            );
+        }
     }
 
     private create(port: number) {
@@ -98,12 +133,14 @@ export class Edno {
 
                     overrideReq.params = m ? m.groups : undefined;
                     overrideReq.body = await readBody(req);
-                    for (let mid = 0; mid < middlewares.length; mid++) {
-                        await processMiddleware(
-                            middlewares[mid],
-                            overrideReq,
-                            ResponseBuilder(<Response>res)
-                        );
+                    if(middlewares){
+                        for (let mid = 0; mid < middlewares.length; mid++) {
+                            await processMiddleware(
+                              middlewares[mid],
+                              overrideReq,
+                              ResponseBuilder(<Response>res)
+                            );
+                        }
                     }
                     const overrideRes = ResponseBuilder(<Response>res);
                     cb(req, overrideRes);
@@ -116,24 +153,13 @@ export class Edno {
                 res.end('not found');
             }
         }).listen(port);
-
-        return {
-            get: (path: string, ...rest: any[]) => {
-                this.methodFunction('get', path, ...rest);
-            },
-            post: (path: string, ...rest: any[]) => {
-                this.methodFunction('post', path, ...rest);
-            },
-            put: (path: string, ...rest: any[]) => {
-                this.methodFunction('put', path, ...rest);
-            },
-            delete: (path: string, ...rest: any[]) => {
-                this.methodFunction('delete', path, ...rest);
-            },
-        };
     }
 
-    public start(cb?: () => void): void{
+    private createRoute(method: string, path: string, ...rest: any[]) {
+        this.methodFunction(method, path, ...rest);
+    }
+
+    public start(cb?: () => void): void {
         cb ? cb() : undefined;
         this.create(this.options.port);
     }
