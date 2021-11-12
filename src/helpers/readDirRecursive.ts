9951079fe28a891e2b9982744287cd1c6401d055
Blopaa <pabloosabaterr@gmail.com>
2021-11-12 17:42:14 +0100

    hotfix: error thrown with missing files

--- src/helpers/readDirRecursive.ts
+++ src/helpers/readDirRecursive.ts
@@ -1,4 +1,4 @@
-import { readdirSync, statSync } from "fs";
+import { existsSync, readdirSync, statSync } from "fs";
 import path from "path";
 
 /**
@@ -7,14 +7,16 @@ import path from "path";
  */
 export function readDirRecursive(dir: string): string[] {
     const files: string[] = [];
-    const list = readdirSync(dir);
-    for (let x = 0; x < list.length; x++) {
-        const filePath = path.join(dir, list[x]);
-        const stats = statSync(filePath);
-        if (stats.isDirectory()) {
-            files.push(...readDirRecursive(filePath));
-        } else {
-            files.push(filePath);
+    if(existsSync(dir)){
+        const list = readdirSync(dir);
+        for (let x = 0; x < list.length; x++) {
+            const filePath = path.join(dir, list[x]);
+            const stats = statSync(filePath);
+            if (stats.isDirectory()) {
+                files.push(...readDirRecursive(filePath));
+            } else {
+                files.push(filePath);
+            }
         }
     }
     return files;
