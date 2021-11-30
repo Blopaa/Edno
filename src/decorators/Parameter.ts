98be59eb3087825d11c94de7bfb720de4b4fa7a6
Blopaa <pabloosabaterr@gmail.com>
2021-11-30 22:28:32 +0100

    feat: parameter decorators

---
+++ src/decorators/Parameter.ts
@@ -0,0 +1,53 @@
+import parameterStore from "../stores/ParameterStore";
+import { ParamTypes } from "../types";
+
+export function Param(details: string): ParameterDecorator {
+    return (
+        target: object,
+        propertyKey: string | symbol,
+        parameterIndex: number
+    ) => {
+        parameterStore.registerParameter({
+            name: details,
+            value: undefined,
+            type: ParamTypes.PARAM,
+            index: parameterIndex,
+            propertyKey: propertyKey.toString(),
+            target: target.constructor.name,
+        });
+    };
+}
+
+export function Res(): ParameterDecorator {
+    return (
+        target: object,
+        propertyKey: string | symbol,
+        parameterIndex: number
+    ) => {
+        parameterStore.registerParameter({
+            name: "",
+            value: undefined,
+            type: ParamTypes.RESPONSE,
+            index: parameterIndex,
+            propertyKey: propertyKey.toString(),
+            target: target.constructor.name,
+        });
+    };
+}
+
+export function Body(details: string): ParameterDecorator {
+    return (
+        target: object,
+        propertyKey: string | symbol,
+        parameterIndex: number
+    ) => {
+        parameterStore.registerParameter({
+            name: details,
+            value: undefined,
+            type: ParamTypes.BODY,
+            index: parameterIndex,
+            propertyKey: propertyKey.toString(),
+            target: target.constructor.name,
+        });
+    };
+}
